95f84cab25f4c8c2e912868b50346f97bcdf0b92
anmaloma <106412980+anmaloma@users.noreply.github.com>
2026-06-15 19:21:50 -0600

    Delete finalproyect directory

--- finalproyect/scripts/menu.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector("#menu-button");
-const navigation = document.querySelector("#navigation");
-
-menuButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-});
